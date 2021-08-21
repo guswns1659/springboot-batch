7dd41118034fefd274582ce8989def8d3a959d82
guswns1659 <guswns1659@gmail.com>
2021-08-21 23:18:22 +0900

    Complete setting of BatchConfiguration

--- src/main/java/com/titanic/springbootbatch/BatchConfiguration.java
+++ src/main/java/com/titanic/springbootbatch/BatchConfiguration.java
@@ -2,9 +2,12 @@ package com.titanic.springbootbatch;
 
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
@@ -51,10 +54,34 @@ public class BatchConfiguration {
 
     @Bean
     public JdbcBatchItemWriter<Person> writer(DataSource dataSource) {
-        return new JdbcBatchItemWriterBuilder<Person>().itemSqlParameterSourceProvider(
-            new BeanPropertyItemSqlParameterSourceProvider<>())
+        return new JdbcBatchItemWriterBuilder<Person>()
+            .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
             .sql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)")
             .dataSource(dataSource)
             .build();
     }
+
+    // TODO : 병렬실행 해보기
+    @Bean
+    public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
+        return jobBuilderFactory.get("importUserJob")
+            .incrementer(new RunIdIncrementer())
+            .listener(listener)
+            .flow(step1)
+            .end()
+            .build();
+
+    }
+
+    @Bean
+    public Step step1(JdbcBatchItemWriter<Person> writer) {
+        // Q : get("step1")을 변경하면 Job에서 못찾나?
+        return stepBuilderFactory.get("step1")
+            .<Person, Person> chunk(10)
+            .reader(reader())
+            .processor(processor())
+            .writer(writer)
+            .build();
+    }
+
 }
