4e3843b9a7498b90d5b212b9394adef8bd5f9869
guswns1659 <guswns1659@gmail.com>
2021-08-21 22:53:02 +0900

    feat: Create FlatFileItemReader

--- src/main/java/com/titanic/springbootbatch/BatchConfiguration.java
+++ src/main/java/com/titanic/springbootbatch/BatchConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,11 +27,16 @@ public class BatchConfiguration {
         this.stepBuilderFactory = stepBuilderFactory;
     }
 
-//    @Bean
-//    public FlatFileItemReader<Person> reader() {
-//        return new FlatFileItemReader<Person>()
-//            .setResource(new ClassPathResource("sample-data.csv"))
-//            .setName("personItemReader")
-//            .
-//    }
+    @Bean
+    public FlatFileItemReader<Person> reader() {
+        return new FlatFileItemReaderBuilder<Person>()
+            .name("personItemReader")
+            .resource(new ClassPathResource("sample-data.csv"))
+            .delimited()
+            .names("firstName", "lastName")
+            .fieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
+                setTargetType(Person.class);
+            }})
+            .build();
+    }
 }
